ba9b07aca53979555047f382dff6bd5e7ae078f0
Damodar Khatiwada <damodar_khatiwada@hotmail.com>
2023-05-11 09:00:21 -0500

    ADDed code

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using testsqlappcloud.Models;
+using testsqlappcloud.Services;
+
+namespace testsqlappcloud.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public List<Product> Products;
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            ProductService productService = new ProductService();
+
+            Products = productService.GetProducts();
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
